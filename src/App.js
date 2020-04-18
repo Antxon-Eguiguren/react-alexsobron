fd672193b7800139e752e38f32eec06b132d5b54
Antxon Eguiguren <antxonegi@gmail.com>
2020-04-18 18:37:16 +0200

    RandomCanvas component and Route created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import RandomCanvas from './components/random-canvas/random-canvas.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      mediaFromWP: []
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8888/alexsobron/back/wp-json/wp/v2/media?per_page=100');
+    const data = await response.json();
+    this.setState({ mediaFromWP: data });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Switch>
+          <Route exact path='/canvas' render={() => (<RandomCanvas media={this.state.mediaFromWP} />)} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
